97d9bba6429411b994652de6b0db2213f2636993
Jared Fuller <jaredshane@gmail.com>
2017-10-06 12:15:20 -0500

    started on the coins-to-cash exercise

---
+++ book-2-the-novice/coins-to-cash/coinsToCash.js
@@ -0,0 +1,18 @@
+const piggyBank = { 
+  pennies: 342,
+  nickels: 124,
+  dimes: 55,
+  quarters: 72
+ }
+
+ let dollarAmount = 0
+
+ let calculateMoney = () => {
+   dollarAmount += piggyBank.pennies * .01
+   dollarAmount += piggyBank.nickels * .05
+   dollarAmount += piggyBank.dimes * .1
+   dollarAmount += piggyBank.quarters * .25
+   console.log(dollarAmount.toFixed(2))
+ }
+
+ calculateMoney()
