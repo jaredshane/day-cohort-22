2bf9c741bdd1d60212d7637bf6a3a06009d64b8b
Jared Fuller <jaredshane@gmail.com>
2017-10-16 13:47:56 -0500

    made some changes to cash to coins exercise

--- book-2-the-novice/coins-to-cash/coinsToCash.js
+++ book-2-the-novice/coins-to-cash/coinsToCash.js
@@ -1,18 +1,28 @@
-const piggyBank = { 
+const piggyBank = {
   pennies: 342,
   nickels: 124,
   dimes: 55,
   quarters: 72
- }
+}
 
- let dollarAmount = 0
+let dollarAmount = 0
 
- let calculateMoney = () => {
-   dollarAmount += piggyBank.pennies * .01
-   dollarAmount += piggyBank.nickels * .05
-   dollarAmount += piggyBank.dimes * .1
-   dollarAmount += piggyBank.quarters * .25
-   console.log(dollarAmount.toFixed(2))
- }
+let calculateMoney = () => {
+  dollarAmount += piggyBank.pennies * .01
+  dollarAmount += piggyBank.nickels * .05
+  dollarAmount += piggyBank.dimes * .1
+  dollarAmount += piggyBank.quarters * .25
+  console.log(dollarAmount.toFixed(2))
+}
 
- calculateMoney()
+calculateMoney()
+
+
+
+function calculateMoney() {
+  dollarAmount += piggyBank.pennies * .01
+  dollarAmount += piggyBank.nickels * .05
+  dollarAmount += piggyBank.dimes * .1
+  dollarAmount += piggyBank.quarters * .25
+  console.log(dollarAmount.toFixed(2))
+}
