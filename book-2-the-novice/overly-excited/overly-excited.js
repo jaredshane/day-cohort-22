cf9e20accc03b8bea49fecc5f7441e40a1248b36
Jared Fuller <jaredshane@gmail.com>
2017-11-03 14:58:57 -0500

    commenting and uncommenting some code

--- book-2-the-novice/overly-excited/overly-excited.js
+++ book-2-the-novice/overly-excited/overly-excited.js
@@ -1,35 +1,40 @@
 // Create an array that contains the words in the sentence
-// let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
+let sentence = ['The', 'walrus', 'danced', 'through', 'the', 'trees', 'in', 'the', 'light', 'of', 'the', 'moon']
 
-// /*
-//     The addExcitement function should be an impure function, and accept 
-//     the array as the sole argument. It should iterate over the array 
-//     and output the words to the browser console.
-// */
+/*
+    The addExcitement function should be an impure function, and accept 
+    the array as the sole argument. It should iterate over the array 
+    and output the words to the browser console.
+*/
 // function addExcitement (theWordArray) {
 //   let sentenceToWrite = ""
 //   for (let i = 0; i < theWordArray.length; i++) {
 //     let currentWord = theWordArray[i]
 //     sentenceToWrite += currentWord + " "
-//     console.log(sentence)
+//     console.log(sentenceToWrite)
 //   }
 // }
-
-// for
-// // Invoke the function and pass in the array
+// Invoke the function and pass in the array
 // addExcitement(sentence);
 
+// let sentenceToPrint = ''
+// let counter = 0
+// while (counter < sentence.length) {
+//   sentenceToPrint += sentence[counter] + " "
+//   counter++
+//   console.log(sentenceToPrint)
+// }
 
 
 // Create an array that contains the words in the sentence
 // const sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
 // const exclamation = "!"
 
-// /*
-//     The addExcitement function should be an impure function, and accept 
-//     the array as the sole argument. It should iterate over the array 
-//     and output the words to the browser console.
-// */
+/*
+    The addExcitement function should be an impure function, and accept 
+    the array as the sole argument. It should iterate over the array 
+    and output the words to the browser console.
+*/
 // function addExcitement (theWordArray) {
 //   let sentenceToWrite = ""
 //   for (let i = 0; i < theWordArray.length; i++) {
@@ -47,6 +52,27 @@
 
 // // Invoke the function and pass in the array
 // addExcitement(sentence);
+
+// let sentenceToPrint = ''
+// let counter = 0
+// while (counter < sentence.length) {
+//    let currentWord = sentence[counter]
+//     let newCounter = counter + 1
+//   if (newCounter % 3 === 0) {
+//     sentenceToPrint += currentWord + exclamation + " "
+//     counter++
+//   } else {
+//     sentenceToPrint += currentWord + " "
+//     counter++
+//   }
+//   console.log(sentenceToPrint)
+// }
+
+// while (i < sentence.length) {
+//   if (i % 3 === 0) {
+    
+//   }
+// }
 
 
 // // Create an array that contains the words in the sentence
@@ -79,33 +105,33 @@
 
 
 
-// Create an array that contains the words in the sentence
-const sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
+// // Create an array that contains the words in the sentence
+// const sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
 
 
-/*
-    The addExcitement function should be an impure function, and accept 
-    the array as the sole argument. It should iterate over the array 
-    and output the words to the browser console.
-*/
+// /*
+//     The addExcitement function should be an impure function, and accept 
+//     the array as the sole argument. It should iterate over the array 
+//     and output the words to the browser console.
+// */
 let addExcitement = (theWordArray, character) => {
-  let sentence = ""
-  let characterList = character
-  for (let i = 0; i < theWordArray.length; i++) {
-    let currentWord = theWordArray[i]
-    let counter = i + 1
-   if (counter % 3 === 0) {
-      currentWord += characterList + " "
-      characterList += character
-    } else {
-      currentWord += " "
-    }
-     sentence += currentWord + " "
-     console.log(sentence)
-   }
- }
+	let sentence = ''
+	let characterList = character
+	for (let i = 0; i < theWordArray.length; i++) {
+		let currentWord = theWordArray[i]
+		let counter = i + 1
+		if (counter % 3 === 0) {
+			currentWord += characterList + ' '
+			characterList += character
+		} else {	
+			currentWord += ' '
+		}
+		sentence += currentWord + ' '
+		console.log(sentence)
+	}
+}
 
-// // Invoke the function and pass in the array
- addExcitement(sentence, "?");
-//  addExcitement(sentence, "*")
-//  addExcitement(sentence, "@")
+// // // Invoke the function and pass in the array
+//  addExcitement(sentence, "?");
+addExcitement(sentence, '*')
+addExcitement(sentence, '@')
