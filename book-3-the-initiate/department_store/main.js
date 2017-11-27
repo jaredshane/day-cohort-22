155c28884e36ef168ad30adadc6f5409e639943a
Jared Fuller <jaredshane@gmail.com>
2017-11-27 09:19:51 -0600

    added some spacing and ajax requests for the department store

--- book-3-the-initiate/department_store/main.js
+++ book-3-the-initiate/department_store/main.js
@@ -3,6 +3,22 @@ $(document).ready(() => {
 	let cats = []
 	let prods = []
 
+	
+
+// 	$.ajax({
+//        url: 'https://new-cool-project-7d0fa.firebaseio.com/.json',
+//        type: "GET",
+//        cache: false,
+//        dataType: "jsonp",
+//        success : function(result){console.log(result);},
+//        error : function(error){console.log(error);}
+// });
+
+$.ajax('https://new-cool-project-7d0fa.firebaseio.com/.json')
+.then(result => {
+	console.log(result)
+})
+
 	$.ajax('products.json')
 		.then(products => {
 		
@@ -14,47 +30,62 @@ $(document).ready(() => {
 				prods = products.products
 
 				outputEl.html(writeToDom(cats, prods))
+				return 10
+			})
+			.then((stuff) => {
+				console.log(stuff)
 			})
 		})
 
 		$('select').change((e) => {
 			outputEl.html(writeToDom(cats, prods))
-			let seasonSelected = e.target.value
-			if (seasonSelected === "none") {
+			let categorySelected = e.target.value
+			if (categorySelected === "none") {
 				return
 			}
 			let discountFound = cats.find((cat) => {
-				return cat.name === seasonSelected
+				return cat.name === categorySelected
 			})
-			let selectedCategory = Array.from($(`.${seasonSelected}`))
+			console.log(discountFound)
+			let selectedCategory = Array.from($(`.${categorySelected}`))
 			let priceEl = Array.from($(`.price__${discountFound.id}`))
+			
 			priceEl.forEach(el => {
 				let currentPrice = el.innerHTML
 				currentPrice = currentPrice - (currentPrice * discountFound.discount)
 				el.innerHTML = currentPrice.toFixed(2)
 			})
 
-			
+		
 		})
 
 })
 
 let writeToDom = (cats, prods) => {
+
 	let htmlString = ""
+
 	cats.forEach(cat => {
+
 		htmlString += 
 		`<div>
 			<h1>${cat.name}</h1>`
+
 		prods.forEach(prod => {
+
 			if (cat.id == prod.category_id) {
+
 				htmlString += `
 				<section class='${cat.name}'>
 					<div>${prod.name} </div>
 					<div class="price__${prod.category_id}">${prod.price}</div>
 				</section>`
+
 			}
 		})
+
 		htmlString += "</div>"
 	})
+
 	return htmlString
 }
